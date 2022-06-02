3f8215726efc3229e1277b87d0930e5d6065c9cc
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-06-02 17:51:36 +0900

    3-04_comparison

---
+++ 0_practice/3-04_comparison/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const number = Math.floor(Math.random() * 6);
+const answer = parseInt(window.prompt('数当てゲーム。0~5の数字を入力してね。'));
+let message;
+if(answer === number){
+  message = '当たり！';
+} else if(answer < number) {
+  message = '残念！数字はもっと大きい';
+} else if(answer > number) {
+  message = '残念！数字はもっと小さい';
+} else {
+  message = '0~5の数字を入力してね。';
+}
+window.alert(message);
