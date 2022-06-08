9cb8e4e48c313230382eb94938aa50356e89433a
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-06-08 14:56:34 +0900

    5-03_cookie

---
+++ 0_practice/5-03_cookie/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const agree = Cookies.get('cookie-agree');
+const panel = document.getElementById('privacy-panel');
+if(agree === 'yes') {
+  document.body.removeChild(panel);
+} else {
+  document.getElementById('agreebtn').onclick = function() {
+    Cookies.set('cookie-agree', 'yes',{expires: 7});
+    document.body.removeChild(panel);
+  };
+}
+
+// クッキー削除
+document.getElementById('testbtn').onclick = function() {
+  Cookies.remove('cookie-agree');
+};
